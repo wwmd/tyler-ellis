d53aee4c940432aabd7b1632f8ceb4ad58ae0833
jasongrier <jason@hemberlin.de>
2017-06-19 01:15:17 +0200

    Dev setup.

--- theme/gulpfile.js
+++ theme/gulpfile.js
@@ -1,19 +1,44 @@
-// Gulp plugin setup
 var gulp = require('gulp');
-
-// Watches single files
 var watch = require('gulp-watch');
 var gulpShopify = require('gulp-shopify-upload');
+var argv = require('yargs').argv;
+var devConfig = require('./config/deploy/devConfig.json');
+var stagingConfig = require('./config/deploy/stagingConfig.json');
+var presentationConfig = require('./config/deploy/presentationConfig.json');
+var liveConfig = require('./config/deploy/liveConfig.json');
 
-// Grabs your API credentials
-var config = require('./config.json');
+var environment = argv.env || 'dev'
+
+var credentials
+
+switch (environment) {
+  case 'staging':
+    credentials = stagingConfig
+    break;
+
+  case 'presentation':
+    credentials = presentationConfig
+    break;
+
+  case 'live':
+    credentials = liveConfig
+    break;
+
+  default:
+    credentials = devConfig
+    break;
+}
+
 gulp.task('shopifywatch', function() {
 
-return watch('./theme/+(assets|layout|config|sections|snippets|templates|locales)/**')
- .pipe(gulpShopify(config.shopify_api_key, config.shopify_api_password, config.shopify_url, null));
+return watch('**')
+ .pipe(gulpShopify(
+   credentials.shopify_api_key,
+   credentials.shopify_api_password,
+   credentials.shopify_url
+  ));
 });
 
-// Default gulp action when gulp is run
 gulp.task('default', [
   'shopifywatch'
 ]);

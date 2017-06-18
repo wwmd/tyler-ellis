51eb5533926413c2a6614406d5fc1134b8b17131
jasongrier <jason@hemberlin.de>
2017-06-18 23:38:22 +0200

    Set up for Gulp.

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// Gulp plugin setup
+var gulp = require('gulp');
+
+// Watches single files
+var watch = require('gulp-watch');
+var gulpShopify = require('gulp-shopify-upload');
+
+// Grabs your API credentials
+var config = require('./config.json');
+gulp.task('shopifywatch', function() {
+var options = {
+  'basePath': './theme/'
+};
+
+return watch('./theme/+(assets|layout|config|sections|snippets|templates|locales)/**')
+ .pipe(gulpShopify(config.shopify_api_key, config.shopify_api_password, config.shopify_url, null, options));
+});
+
+// Default gulp action when gulp is run
+gulp.task('default', [
+  'shopifywatch'
+]);
